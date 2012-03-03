7e8ab3930f078405566d22596e52245a956c0eb6
mouseplatterman <rattray.alex@gmail.com>
2012-03-03 16:16:08 -0500

    made the django app a subdir

---
+++ web/app/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.defaults import *
+from app.views import *
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    #important general shit
+    #(r'^admin/', include(admin.site.urls)),
+    (r'media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+
+    #standard pages    
+    (r'^$', index),    
+    )
