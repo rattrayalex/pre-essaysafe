5a18316dbfb9f41f0037b466726cfee9202b9cef
unknown <Greg Terrono@GregTerrono.(none)>
2012-03-04 17:43:56 -0500

    Desktop client finished for demo

---
+++ desktop/EssaySafeDesktopClient/EssaySafeDesktopClient/GeneratedFiles/Debug/moc_MyText.cpp
@@ -0,0 +1,104 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'MyText.h'
+**
+** Created: Sun Mar 4 17:37:41 2012
+**      by: The Qt Meta Object Compiler version 63 (Qt 4.8.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../MyText.h"
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'MyText.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 63
+#error "This file was generated using the moc from 4.8.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+static const uint qt_meta_data_MyText[] = {
+
+ // content:
+       6,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: signature, parameters, type, tag, flags
+       8,    7,    7,    7, 0x05,
+
+ // slots: signature, parameters, type, tag, flags
+      26,    7,    7,    7, 0x0a,
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_MyText[] = {
+    "MyText\0\0sendText(QString)\0getText()\0"
+};
+
+void MyText::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
+        MyText *_t = static_cast<MyText *>(_o);
+        switch (_id) {
+        case 0: _t->sendText((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 1: _t->getText(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObjectExtraData MyText::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
+const QMetaObject MyText::staticMetaObject = {
+    { &QLineEdit::staticMetaObject, qt_meta_stringdata_MyText,
+      qt_meta_data_MyText, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &MyText::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *MyText::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *MyText::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_MyText))
+        return static_cast<void*>(const_cast< MyText*>(this));
+    return QLineEdit::qt_metacast(_clname);
+}
+
+int MyText::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QLineEdit::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void MyText::sendText(QString _t1)
+{
+    void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+QT_END_MOC_NAMESPACE
