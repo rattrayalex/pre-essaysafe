411f81438051c7c0f5825c20be12d09b76242500
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-03-05 22:12:27 -0500

    fixed header link

---
+++ models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib import admin
+import datetime
+from django.db.models.signals import post_save
+from django.core.files import File
+import os.path
+import djangotoolbox.fields as models2
+from django.template.defaultfilters import slugify
+
+
+class Professor(models.Model):
+  """ 
+  Professor model.
+  """
+  email = models.CharField(max_length=80)
+  box_id = models.CharField()
+  gdocs_id = models.CharField()
+  user = models.OneToOneField(User, unique=True)
+  name = models.CharField()
+  folder_id = models.CharField(max_length=80, blank=True)
+  token = models.CharField(max_length=100, blank=True)
+  token_secret = models.CharField(max_length=100, blank=True)
+  ##auth_token = models.CharField(max_length=100, blank=True)
+  
+  def __unicode__(self):
+    return self.name
+  
+class Exam(models.Model):
+  """ Exam model.
+  """
+  professor = models.ForeignKey(Professor, null=True)
+  name = models.CharField(max_length=80, unique=True)
+  start_time = models.DateTimeField(blank=True)
+  end_time = models.DateTimeField(blank=True)
+  resource_id = models.CharField(max_length=80)
+  folder_id = models.CharField(max_length=80)
+  box_fid = models.CharField(max_length=100)
+  box_email = models.CharField(max_length=100)
+  
+  def __unicode__(self):
+    return self.name
+
+class Essay(models.Model):
+  exam = models.ForeignKey(Exam, blank=True, null=True)
+  student_name = models.CharField(max_length=80, blank=True)
+  student_email = models.CharField(max_length=80, blank=True)
+  start_date = models.DateTimeField('date finished', blank=True, null=True)
+  end_date = models.DateTimeField('date finished', blank=True, null=True)
+  latitude = models.DecimalField(blank=True, null=True)
+  longitude = models.DecimalField(blank=True, null=True)
+  resource_id = models.CharField(blank=True, null=True)
+
+class Doc(models.Model):
+  doc_name = models.CharField(max_length=80)
+  resource_id = models.CharField(max_length=80)
+
